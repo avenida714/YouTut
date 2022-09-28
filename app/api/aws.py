2b56815d960ddc75a0ce0a0f3c1b0f452cef1391
Alec Venida <alecvenida@gmail.com>
2022-09-28 15:41:34 -0700

    use os.environ to get bucket name; use this to define location; create func to upload the files

--- app/api/aws.py
+++ app/api/aws.py
@@ -3,7 +3,14 @@ import botocore
 import os
 import uuid
 
+#define which extensions are allowed
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+
+#get s3 name from the os environment; should be youtut
+BUCKET_NAME = os.environ.get("S3_BUCKET")
+
+#define the location based on the s3 name
+S3_LOCATION = f"http://{BUCKET_NAME}.s3.amazonaws.com/"
 
 s3 = boto3.client(
    "s3",
@@ -20,3 +27,21 @@ def get_unique_filename(filename):
     ext = filename.rsplit(".", 1)[1].lower()
     unique_filename = uuid.uuid4().hex
     return f"{unique_filename}.{ext}"
+
+
+def upload_file_to_s3(file, acl="public-read"):
+    try:
+        s3.upload_fileobj(
+            file,
+            BUCKET_NAME,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+    except Exception as e:
+        # in case the our s3 upload fails
+        return {"errors": str(e)}
+
+    return {"url": f"{S3_LOCATION}{file.filename}"}
