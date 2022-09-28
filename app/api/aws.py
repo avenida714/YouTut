f6992a08eec12f10f1df484ed7fb5248e12faac0
Alec Venida <alecvenida@gmail.com>
2022-09-28 15:38:18 -0700

    define allowed extensions; import uuid; ensure files are uniquely named

--- app/api/aws.py
+++ app/api/aws.py
@@ -1,9 +1,22 @@
 import boto3
 import botocore
 import os
+import uuid
+
+ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 
 s3 = boto3.client(
    "s3",
    aws_access_key_id=os.environ.get("S3_KEY"),
    aws_secret_access_key=os.environ.get("S3_SECRET")
 )
+
+
+def allowed_file(filename):
+    return "." in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_unique_filename(filename):
+    ext = filename.rsplit(".", 1)[1].lower()
+    unique_filename = uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
