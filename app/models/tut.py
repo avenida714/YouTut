63157f155f00de1a61d05cf1a420500b533b356a
Alec Venida <alecvenida@gmail.com>
2022-09-29 09:23:56 -0700

    import datetime for created at; need to find a default thumbnail

--- app/models/tut.py
+++ app/models/tut.py
@@ -1,5 +1,5 @@
 from .db import db
-# from datetime import datetime
+from datetime import datetime
 
 class Tut(db.Model):
   __tablename__ = 'Tuts'
@@ -7,8 +7,8 @@ class Tut(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
   tut_title = db.Column(db.String(100), nullable=False)
-  tut_description = db.Column(db.String(1000), nullable=False)
-  created_at
-  updated_at
-  thumbnail_pic
+  tut_description = db.Column(db.Text, nullable=False)
+  created_at = db.Column(db.DateTime, nullable=False, default=(datetime.now()))  # import datetime for this
+  updated_at = db.Column(db.DateTime, nullable=False)
+  thumbnail_pic = (db.Column(db.Text, nullable=True))  #need to add a default photo so there won't be broken images
   tut_data
