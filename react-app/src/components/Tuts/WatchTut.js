3a507a8e583c3e062af6c2560c78da27956f5ed2
Alec Venida <alecvenida@gmail.com>
2022-09-29 15:12:23 -0700

    testing the video with a watchtut component

---
+++ react-app/src/components/Tuts/WatchTut.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function WatchTut() {
+  return (
+    <div>
+      <video src="https://youtut.s3.us-west-1.amazonaws.com/What+is+IPFS+_+720p.mp4" controls/>
+    </div>
+  )
+}
+
+export default WatchTut
