c4aa7e22699cfbe6a60ee6d150cd704657ba9bb8
Alec Venida <alecvenida@gmail.com>
2022-09-29 09:18:55 -0700

    working on Tut DB model

--- app/models/tut.py
+++ app/models/tut.py
@@ -3,3 +3,12 @@ from .db import db
 
 class Tut(db.Model):
   __tablename__ = 'Tuts'
+
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+  tut_title = db.Column(db.String(100), nullable=False)
+  tut_description = db.Column(db.String(1000), nullable=False)
+  created_at
+  updated_at
+  thumbnail_pic
+  tut_data
