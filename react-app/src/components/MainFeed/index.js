d2147f0b7d395c518686cb66bccc25fed2977af5
Alec Venida <alecvenida@gmail.com>
2022-09-30 09:52:53 -0700

    rename mainfeed component

--- react-app/src/components/MainFeed/index.js
+++ react-app/src/components/MainFeed/index.js
@@ -2,10 +2,10 @@
 
 import React from 'react'
 
-function index() {
+function MainFeed() {
   return (
-    <div>index</div>
+    <div>MainFeed</div>
   )
 }
 
-export default index
+export default MainFeed
