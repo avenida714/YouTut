adf20345ad34db6ed6708e13adcb038a3db3dfa0
Alec Venida <alecvenida@gmail.com>
2022-10-05 13:45:16 -0700

    starting comments store; action types

---
+++ react-app/src/store/comments.js
@@ -0,0 +1,4 @@
+const CREATE_COMMENT = "comment/createComment";
+const READ_COMMENT = "comment/readComment"; // get all comments action type
+const UPDATE_COMMENT = "comment/updateComment";
+const DELETE_COMMENT = "comment/deleteComment";
