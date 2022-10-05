ab52421942f571e2de7be05fbf486be9fb519808
Alec Venida <alecvenida@gmail.com>
2022-10-05 11:07:47 -0700

    added comments to __init__.py main in app

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.tut_routes import tut_routes
+from .api.comment_routes import comment_routes
 
 from .seeds import seed_commands
 
