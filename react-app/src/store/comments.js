d69c8bebb89ba8f2137514a3562dee1b75a021ee
Alec Venida <alecvenida@gmail.com>
2022-10-05 13:46:34 -0700

    action creators for comments

--- react-app/src/store/comments.js
+++ react-app/src/store/comments.js
@@ -2,3 +2,31 @@ const CREATE_COMMENT = "comment/createComment";
 const READ_COMMENT = "comment/readComment"; // get all comments action type
 const UPDATE_COMMENT = "comment/updateComment";
 const DELETE_COMMENT = "comment/deleteComment";
+
+// ACTION CREATORS ----------------
+const actionCreateComment = (comment) => {
+  return {
+    type: CREATE_COMMENT,
+    comment,
+  };
+};
+const actionReadComment = (comment) => {
+  return {
+    type: READ_COMMENT,
+    comment,
+  };
+};
+const actionUpdateComment = (comment) => {
+  return {
+    type: UPDATE_COMMENT,
+    comment,
+  };
+};
+const actionDeleteComment = (id) => {
+  return {
+    type: DELETE_COMMENT,
+    id,
+  };
+};
+
+//THUNKS -------------------
