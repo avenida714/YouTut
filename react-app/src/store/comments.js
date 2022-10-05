2c844176e5857deda530e6905c6c4b5de3e9c3c9
Alec Venida <alecvenida@gmail.com>
2022-10-05 14:03:21 -0700

    create comment in comment store

--- react-app/src/store/comments.js
+++ react-app/src/store/comments.js
@@ -1,3 +1,5 @@
+import { getOneTutById } from "./tuts";
+
 const CREATE_COMMENT = "comment/createComment";
 const READ_COMMENT = "comment/readComment"; // get all comments action type
 const UPDATE_COMMENT = "comment/updateComment";
@@ -38,6 +40,25 @@ export const getTutComments = (tutId) => async (dispatch) => {
   if (response.ok) {
     const comments = await response.json();
     dispatch(actionReadComment(comments.comments));
+  }
+  return response;
+};
+
+// Create a comment for tut
+export const createComment = (newCommentData) => async (dispatch) => {
+  const response = await fetch(
+    `/api/tuts/${newCommentData.tut_id}/new_comment`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newCommentData),
+    }
+  );
+  if (response.ok) {
+    const newComment = await response.json();
+    await dispatch(actionCreateComment(newComment));
+    await dispatch(getOneTutById(newCommentData.tut_id));
+    return newComment;
   }
   return response;
 };
