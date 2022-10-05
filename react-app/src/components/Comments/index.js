4c4997084eab44c8f7925782a3e11afb61c7a000
Alec Venida <alecvenida@gmail.com>
2022-10-05 15:25:33 -0700

    comments component

---
+++ react-app/src/components/Comments/index.js
@@ -0,0 +1,48 @@
+//Comments
+
+import React from "react";
+
+function Comments({ tut }) {
+  let loopMe;
+
+  if (tut.comments) loopMe = true;
+  else loopMe = false;
+
+  return (
+    loopMe && (
+      <div>
+        <div className="caption-comments">
+          <div className="comment-display-watchTut">
+            <div className="tut-caption-watchTut">{loopMe.caption}</div>
+            {loopMe.comments.map((comment) => (
+              <div className="comment_line" key={comment.id}>
+                <div className="user-icon-watchTut-comment">
+                  <img
+                    alt="profile-pic"
+                    className="img circle comment-img"
+                    src={comment.user.profile_img}
+                  />
+                </div>
+
+                <div className="bold">{comment.user.username}:</div>
+                <div className="comment_content"> {comment.comment}</div>
+                {/* <div className="comment_content">
+                        {comment.user.id === currUser.id && (
+                          <EditCommentModal
+                            tut={tut}
+                            comment1={comment.comment}
+                            commentId={comment.id}
+                            type={type}
+                          />
+                        )}
+                      </div> */}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
+
+export default Comments;
