2339943ab732b45696e42c7d0b097bb2f81c911a
Alec Venida <alecvenida@gmail.com>
2022-10-05 22:44:24 -0700

    all create, edit, delete are working but without rerender; edit needs to not be constant; same with delete, it is always there

--- react-app/src/components/Comments/index.js
+++ react-app/src/components/Comments/index.js
@@ -5,47 +5,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { getOneTutById } from "../../store/tuts";
 import "./CommentFeed.css";
 import CreateComment from "./CreateComment";
+import DeleteComment from "./DeleteComment";
 import EditComment from "./EditComment";
 
-
-
-
-
 function CommentFeed({ tut }) {
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   let loopMe;
   // console.log("COMMENTS IN THIS TUT --------", tut.comments)
 
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const userLoggedIn = useSelector((state) => state.session.user);
 
-
-
   // console.log("chrono comments ***********", chronologicalComments)
 
-    useEffect ( () => {
+  useEffect(() => {
+    if (tut.comments) dispatch(getOneTutById(tut.id))
+    .then(() => setIsLoaded(true)
+    )
 
-  if (tut.comments) setIsLoaded(true)
+  }, [dispatch, tut.comments, tut.id]);
 
-    dispatch(getOneTutById(tut.id))
-  }, [dispatch, tut.comments, tut.id])
+  let comments = Object.values(tut.comments);
 
-  let comments = Object.values(tut.comments)
-
-  let chronologicalComments = comments.reverse()
-
-
+  let chronologicalComments = comments.reverse();
 
   return (
     isLoaded && (
       <div>
         <div className="caption-comments">
           <div className="comment-display-watchTut">
-            {<div>
-              <CreateComment tut={tut}/>
-            </div>}
+            {
+              <div>
+                <CreateComment tut={tut} />
+              </div>
+            }
 
             {chronologicalComments.map((comment) => (
               <div className="comment_line" key={comment.id}>
@@ -58,18 +52,21 @@ function CommentFeed({ tut }) {
                 </div>
                 <div className="right-side-comment">
                   <div className="user-name">{comment.user.username}</div>
-                <div className="comment-content"> {comment.comment}</div>
+                  <div className="comment-content"> {comment.comment}</div>
                 </div>
 
                 <div className="comment_content">
-                        {comment.user.id === userLoggedIn.id && (
-                          <EditComment
-                            tut={tut}
-                            oldComment={comment.comment}
-                            commentId={comment.id}
-                          />
-                        )}
-                      </div>
+                  {comment.user.id === userLoggedIn.id && (
+                    <div>
+                      <EditComment
+                        tut={tut}
+                        oldComment={comment.comment}
+                        commentId={comment.id}
+                      />
+                      <DeleteComment commentId={comment.id}/>
+                    </div>
+                  )}
+                </div>
               </div>
             ))}
           </div>
