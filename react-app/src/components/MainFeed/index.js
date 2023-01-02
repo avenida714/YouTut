4481e260467f39b18098ad5ceb8088be2059516d
Alec Venida <alecvenida@gmail.com>
2023-01-02 11:29:24 -0800

    added toLowerCase to all titles etc as well; matching lowercase with lowercase

--- react-app/src/components/MainFeed/index.js
+++ react-app/src/components/MainFeed/index.js
@@ -64,7 +64,7 @@ function MainFeed() {
 
 
 // Search bar logic
-//      const searchTutsResults = tuts.filter(tut => tut.tut_title.includes(searchText.toLowerCase()) || tut.user.username.includes(searchText.toLowerCase()) || tut.tut_description.includes(searchText.toLowerCase()))
+//      const searchTutsResults = tuts.filter(tut => tut.tut_title.toLowerCase().includes(searchText.toLowerCase()) || tut.user.username.toLowerCase().includes(searchText.toLowerCase()) || tut.tut_description.toLowerCase().includes(searchText.toLowerCase()))
 
 /*
 
